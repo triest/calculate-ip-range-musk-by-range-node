231496fb22ac09f20472d32a244cf141f14043b7
triest <you@example.com>
2019-06-15 10:51:23 +0300

    read scv and split

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -7,8 +7,12 @@ const fs = require('fs');
 exports.index=async function (req, res) {
 
     fs.readFile('diapozon.csv', 'utf8', function (err, data) {
-        var dataArray = data.split(/\r?\n/);
-        console.log(dataArray);
+        let dataArray = data.split(/\r?\n/);
+        for(let i=0;i<dataArray.length;i++){
+           let rez=dataArray[i].split(",");
+            console.log(rez[0]);
+            console.log(rez[1]);
+        }
     });
     return res.sendStatus(200);
 };
