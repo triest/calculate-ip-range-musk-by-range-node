a766661fde482da2311cdd8821357c5a9cbe4297
triest <you@example.com>
2019-06-15 15:25:49 +0300

    Add calc mask in loop

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -2,20 +2,29 @@
 
 const fs = require('fs');
 
-
+var IpSubnetCalculator = require( 'ip-subnet-calculator' );
+var rez;
 
 exports.index=async function (req, res) {
 
     fs.readFile('diapozon.csv', 'utf8', function (err, data) {
-        let dataArray = data.split(/\r?\n/);
-        for(let i=0;i<dataArray.length;i++){
-           let rez=dataArray[i].split(",");
-            console.log(rez[0]);
-            console.log(rez[1]);
+         dataArray = data.split(/\r?\n/);
+        for(let i=0;i<dataArray.length-1;i++){
+            rez=dataArray[i].split(",");
+            calcMask(rez[0],rez[1]);
+
         }
     });
     return res.sendStatus(200);
 };
+
+//
+function calcMask(first,last){
+    console.log("first "+first);
+    console.log("last "+last);
+    console.log( IpSubnetCalculator.calculate( first, last ) );
+
+}
 
 
 exports.read=async function (req, res) {
